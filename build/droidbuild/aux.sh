20e6ea0aaf57b29cbe802c9de0f69ca852dea95b
Andrewerr <toddot@live.ru>
2021-11-28 21:51:31 +0200

    [buildsystem]: fixed droidbuild modules collection bugs

--- build/droidbuild/aux.sh
+++ build/droidbuild/aux.sh
@@ -2,11 +2,11 @@
 
 print_info(){
   info "---------------------------------------------------------------------------------"
-  info "Target device:         ${devname}"
+  info "Target device:         ${TARGET_CODENAME}"
   info "Target name:           ${target_name}"
-  info "Build type:            ${buildtype}"
+  info "Build type:            ${TARGET_BUILDTYPE}"
   info "Out directory:         ${out_dir}"
-  info "Keys:                  ${keys}"
+  info "Keys:                  ${TARGET_KEYS}"
   info "Display ID:            ${BUILD_DISPLAY_ID}"
   info "Previous target files: ${previous_target_files}"
   info "---------------------------------------------------------------------------------"
@@ -16,4 +16,13 @@ print_info(){
 # $2 -- directory to search
 get_latest_file(){
    echo `find $2 -name $1 -print0 | xargs -r -0 ls -1 -t | head -1`
+}
+
+# $1 -- pattern to search
+# $2 -- dir name
+# $3 -- ressulting array name
+glob_files(){
+   # FIXME: Use $2	
+   info "glob $2/$1"
+   mapfile -d $'\0' $3 < <(find $2 -wholename "$1" -print0)
 }
