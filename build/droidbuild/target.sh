043f5a4b6cd9c195b60d96ee28bc0b66a1af25a0
Andrewerr <toddot@live.ru>
2021-12-06 16:47:27 +0200

    [manifests] [buildsystem]: disabled bionic, fixed bugs in calling targets

--- build/droidbuild/target.sh
+++ build/droidbuild/target.sh
@@ -1,9 +1,9 @@
 # $1 -- target name
 setup_target(){
-  if ndef DROID_TARGETS; then
-    error "setup_target is called too early. Build system is not yet initialized: can not find DROID_TARGETS!"
-    exit -1
-  fi
+#  if ndef DROID_TARGETS; then
+#    error "setup_target is called too early. Build system is not yet initialized: can not find DROID_TARGETS!"
+#    exit -1
+#  fi
   if ndef "TARGET_$1_INCLUDED"; then
     define "TAREGT_$1_INCLUDED" true
     DROID_TARGETS+=($1)
