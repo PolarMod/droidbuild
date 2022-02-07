a1d900af47ce51e89ec2106626604b06e5cf0280
Andrewerr <p01ar@p01ar.net>
2022-02-07 10:42:41 +0200

    [droidbuild]: fixed bug in setup_target

--- build/droidbuild/target.sh
+++ build/droidbuild/target.sh
@@ -4,8 +4,8 @@ setup_target(){
 #    error "setup_target is called too early. Build system is not yet initialized: can not find DROID_TARGETS!"
 #    exit -1
 #  fi
-  if ndef "TARGET_$1_INCLUDED"; then
-    define "TAREGT_$1_INCLUDED" true
+  if ndef "TARGET_INCLUDED_$1"; then
+    define "TAREGT_INCLUDED_$1" true
     DROID_TARGETS+=($1)
   fi
 }
