b1880594737d7718f427435a5d1784c4eebb0f01
Andrewerr <toddot@protonmail.com>
2022-11-11 16:59:55 +0100

    [FIX] [crypto]: fixed mistake in path to certbundle

--- build/droidbuild/meta.sh
+++ build/droidbuild/meta.sh
@@ -1,6 +1,6 @@
 DROIDBUILD_VERSION="1.2-magika-rc3"
 # In-container config
 DROIDBUILD_CONFIGDIR="/opt/droid/config/"
-DROIDBUILD_OUT_DIR="/opt/droid/out-dir"
+DROIDBUILD_OUT_DIR="$BASEDIR/out_dir"
 DROIDPAK_DIR="/opt/droid/droidpak"
 CERTBUNDLE_PATH="$DROIDBUILD_OUT_DIR/certbundle.zip.sc"
