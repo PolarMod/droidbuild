367e4b4d45e2a0511934bcadc2e495800088c1fa
Andrewerr <toddot@protonmail.com>
2023-09-21 11:46:37 +0200

    [DEPRECATION]: deprecate magika

--- build/droidbuild/banner.sh
+++ build/droidbuild/banner.sh
@@ -6,3 +6,5 @@ info " / _\` | '__/ _ \| |/ _\` | '_ \| | | | | |/ _\` |"
 info '| (_| | | | (_) | | (_| | |_) | |_| | | | (_| |'
 info ' \__,_|_|  \___/|_|\__,_|_.__/ \__,_|_|_|\__,_|'
 info "Version: $DROIDBUILD_VERSION"
+warn "magika version is now deprecated. Use with caution!"
+
